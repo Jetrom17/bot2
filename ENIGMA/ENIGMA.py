2990161cbf2c1138010be948279bd827ee50faa6
Jeiel <Jeiel17@protonmail.com>
2022-11-03 09:46:29 -0300

    Update

--- ENIGMA/ENIGMA.py
+++ ENIGMA/ENIGMA.py
@@ -12,7 +12,31 @@ def example_command(message, cmd):
 @app.route('/bot ?(.*)')
 def example_command2(message, cmd):
     chat_dest = message['chat']['id']
-    msg = "https://jetronix.tk/bot".format(cmd)
+    msg = "https://jetronix.tk/bot.html".format(cmd)
+    app.send_message(chat_dest, msg)
+
+@app.route('/bible ?(.*)')
+def biblie(message, cmd):
+    chat_dest = message['chat']['id']
+    msg = "Via Tor, Google, IPFS: \n https://bible4u.app/".format(cmd)
+    app.send_message(chat_dest, msg)
+
+@app.route('/deepweb ?(.*)')
+def deep(message, cmd):
+    chat_dest = message['chat']['id']
+    msg = "Use Tor: \n http://zqktlwiuavvvqqt4ybvgvi7tyo4hjl5xgfuvpdf6otjiycgwqbym2qad.onion/wiki/index.php/Main_Page".format(cmd)
+    app.send_message(chat_dest, msg)
+
+@app.route('/lofi ?(.*)')
+def lofi(message, cmd):
+    chat_dest = message['chat']['id']
+    msg = "https://app.lofi.co".format(cmd)
+    app.send_message(chat_dest, msg)
+
+@app.route('/livraria ?(.*)')
+def livraria(message, cmd):
+    chat_dest = message['chat']['id']
+    msg = "Via deep web ou Google. Para livros: \n https://1lib.domains/ \n Para artigos: \n https://1lib.domains/?articles".format(cmd)
     app.send_message(chat_dest, msg)
 
 @app.route('/maxplay ?(.*)')
@@ -21,16 +45,34 @@ def example_command3(message, cmd):
     msg = "https://play.google.com/store/apps/details?id=br.com.well.musicas&gl=US".format(cmd)
     app.send_message(chat_dest, msg)
 
+@app.route('/imune ?(.*)')
+def example_command4(message, cmd):
+    chat_dest = message['chat']['id']
+    msg = "https://www.digitbin.com/whatsapp-mod/".format(cmd)
+    app.send_message(chat_dest, msg)
+
+@app.route('/tel ?(.*)')
+def example_command5(message, cmd):
+    chat_dest = message['chat']['id']
+    msg = "https://ddd.guiamais.com.br/".format(cmd)
+    app.send_message(chat_dest, msg)
+
+@app.route('/start ?(.*)')
+def start(message, cmd):
+    chat_dest = message['chat']['id']
+    msg = "Bip Bop! Veja meus outros comandos ou aguarde um atendente:  \n \n https://media.tenor.com/4-063AYho6cAAAAM/robot-goodbye.gif".format(cmd)
+    app.send_message(chat_dest, msg)
+
 
 @app.route('(?!/).+')
 def parrot(message):
    chat_dest = message['chat']['id']
    user_msg = message['text']
 
-   msg = "Saiba mais sobre mim: https://jetronix.tk/bot".format(user_msg)
+   msg = "Doe via pix (Bipa): \n a93ea08c-4239-44d8-8ffa-6a5687b1ed1c \n https://jetronix.tk/".format(user_msg)
    app.send_message(chat_dest, msg)
 
 
 if __name__ == '__main__':
-    app.config['api_key'] = 'My token'
+    app.config['api_key'] = '5472155279:AAEb7mnQjXaJLZmgVzChnEzaaCyRZGYaGM8'
     app.poll(debug=True)
